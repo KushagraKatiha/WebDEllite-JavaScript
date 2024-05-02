2e50f9ecbb63c1608b6d0838b8fe5e9ae6789b48
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-05-03 00:43:45 +0530

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,37 @@
+
+// let lang = "";
+
+// switch (lang) {
+//     case "Hindi":
+//         console.log("Namaste");
+//         break;
+
+//     case "":
+//         console.log("Hello");
+//         break;
+
+//     case "Spanish":
+//         console.log("Hola");
+//         break;
+
+//     default:
+//         console.log("I don't know that language");
+//         break;
+// }
+
+// _______________________Terneray Operator_______________________
+
+
+
+let lang = "1"
+
+;(lang == 1) ? console.log("English") : console.log("Don't know what is it !!!");
+
+
+// let lan = '1';
+// (lan === 1) ? console.log("english") ? console.log("hindi") : console.log("dont know");
+
+let language = ""
+let myVar = "abcd"
+
+;(language == "English") ? console.log("English") : (myVar == "Hindi") ? console.log("Hindi") : console.log("Don't know !!!");
