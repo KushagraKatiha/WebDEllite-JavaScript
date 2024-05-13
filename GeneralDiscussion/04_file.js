e7fc9dc7f36d0ce2dbd578aa38d12930a83eccd6
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-05-13 19:34:44 +0530

    array methods

---
+++ GeneralDiscussion/04_file.js
@@ -0,0 +1,25 @@
+// const array = [15, 16, 17, 18, 19];
+
+// const initialVal = 5;
+// function reducer(accumulator, initialVal, currentValue, index) {
+//     console.log(
+//         `accumulator: ${accumulator}, initialValue: ${initialVal}, currentValue: ${currentValue}, index: ${index},`,
+//     );
+           
+//   return  accumulator + currentValue, initialVal;
+// }
+
+// array.reduce(reducer);
+
+// _________________________________Some___________________________________
+
+let output = ['amit', 'atul', 'nayantiwari', 'wajid', 'kaku'].some((item) => {
+    return item.length > 5
+})
+
+console.log(output);
+
+// ___________________________For in_____________________________________
+
+// ___________________________For of_____________________________________
+
