1a0212a05c031badeb4679bdd10032ed49914f04
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-05-16 23:14:18 +0530

    this keyword

--- Functions/01_function.js
+++ Functions/01_function.js
@@ -1,84 +1,178 @@
-// CSS => Sat, Sun
-// Array, Object, Conditionals, Loops, Variables, Data Types, Opratators
+// // CSS => Sat, Sun
+// // Array, Object, Conditionals, Loops, Variables, Data Types, Opratators
 
-// Function
+// // Function
 
-// Classic => 
-//     function fn_name(params){code}
+// // Classic => 
+// //     function fn_name(params){code}
 
-// Anonymus Function => 
-//     function(params){code}   // function with no name
+// // Anonymus Function => 
+// //     function(params){code}   // function with no name
 
-// Arrow Function/ Fat arrow function => 
+// // Arrow Function/ Fat arrow function => 
     
-//     (params) => {code}
+// //     (params) => {code}
 
-// // IFI, Self invoking function
+// // // IFI, Self invoking function
 
-//     ()()
+// //     ()()
 
 
-// function add(a, b){
-//     let result = a+b
-//     return result 
-// }
+// // function add(a, b){
+// //     let result = a+b
+// //     return result 
+// // }
     
-// let ans1 = add(5, 10)
-// console.log(ans1);
+// // let ans1 = add(5, 10)
+// // console.log(ans1);
 
-// let ans2 = add(7, 8)
-// console.log(ans2);
+// // let ans2 = add(7, 8)
+// // console.log(ans2);
 
-// let ans3 = add(3, 9)
-// console.log(ans3);
+// // let ans3 = add(3, 9)
+// // console.log(ans3);
 
-// // Operator Overloading
-// // let fullName = add("Amit", "Mishra")
-// // console.log(fullName);
+// // // Operator Overloading
+// // // let fullName = add("Amit", "Mishra")
+// // // console.log(fullName);
 
-// let myAdd = function(a, b){
-//     return a+b
+// // let myAdd = function(a, b){
+// //     return a+b
+// // }
+
+// // console.log(myAdd(3, 5))
+
+// // let myNewAdd = (a, b) => {
+// //     return a+b
+// // }
+
+// // console.log(myNewAdd(8, 9));
+
+// function newFunc(a, b, c){
+//     console.log(a, b, c);
+//     let result = a*b*c;
+//     return result;
 // }
 
-// console.log(myAdd(3, 5))
-
-// let myNewAdd = (a, b) => {
-//     return a+b
+// function newFunc2(a, b, c){
+//     console.log(a, b, c);
+//     let result = a+b+c;
+//     return result;
 // }
 
-// console.log(myNewAdd(8, 9));
+// // Function declared latter will be called if it is having same name !!!
 
-function newFunc(a, b, c){
-    console.log(a, b, c);
-    let result = a*b*c;
-    return result;
+// console.log(newFunc(3, 5, 7));
+// console.log(newFunc2(2, 4, 6));
+
+// // ________________________________IIFE___________________________
+// // Immediately Invoked Function Expression
+
+// (function(){
+//     console.log(`I am inside of an IIFE`);
+// })()
+
+
+// ;let result = ((a, b) => {
+//     return a + b;
+// })(5, 6)
+
+// console.log(result);
+
+
+// Write a function to check how many vowels are there in the given string
+
+
+// console.log(myStr.length);
+
+
+// ________________________________________Importance of Return_________________________________
+
+// function coutVowels(str){
+    // let count = 0;
+    // for(let i = str.length-1 ; i>=0; i--){
+        // console.log(j);
+        // if(
+            // str[i] === 'i' || 
+            // str[i] === 'a' || 
+            // str[i] === 'e' || 
+            // str[i] === 'o' || 
+            // str[i] === 'u') {
+            // count++
+        // }
+    // }
+    // console.log(count);
+    // return count
+// }
+// 
+// let newCount = coutVowels("Hello World")
+// 
+// 
+// let twoTimes = (num) => {
+    // console.log(num * 2);
+// }
+// 
+// twoTimes(newCount)
+
+// ____________________________________Types of Return______________________________________
+
+// let times = (num) => {
+//     return num * 2
+// }
+
+// let twoTimes = (num) => num * 2
+
+// console.log(twoTimes(5));
+
+// let numDetails = (name, age) => ({myName: name, myAge: age})
+
+// console.log(numDetails("param", 22))
+
+
+// _______________________________This Keyword________________________________
+
+let myDetails = {
+    firstName: "Amit", 
+    lastName: "chauhan",
+
+    fullName: function(){
+        return this;
+    }
 }
 
-function newFunc2(a, b, c){
-    console.log(a, b, c);
-    let result = a+b+c;
-    return result;
+console.log(myDetails.fullName())
+// {
+//     firstName: "Amit", 
+//     lastName: "chauhan",
+
+//     fullName: function(){
+//         return this;
+//     }
+// }
+
+myDetails.firstName = "Kushagra"
+
+console.log(myDetails.fullName())
+
+// no Change, 
+// {
+    //     firstName: "Kushagra", 
+    //     lastName: "chauhan",
+
+//     fullName: function(){
+//         return this;
+//     }
+// }
+// no change
+
+function func(){
+    console.log(this);
 }
 
-// Function declared latter will be called if it is having same name !!!
+func()
 
-console.log(newFunc(3, 5, 7));
-console.log(newFunc2(2, 4, 6));
+let arrFunc = () => {
+    console.log(this);
+}
 
-// ________________________________IIFE___________________________
-// Immediately Invoked Function Expression
-
-(function(){
-    console.log(`I am inside of an IIFE`);
-})()
-
-
-;let result = ((a, b) => {
-    return a + b;
-})(5, 6)
-
-console.log(result);
-
-
-
-
+arrFunc()
--- Functions/tempCodeRunnerFile.js
+++ Functions/tempCodeRunnerFile.js
@@ -1,5 +1,1 @@
-function newFunc(a, b, c, d){
-    console.log(a, b, c, d);
-    let result = a+b+c+d;
-    return result;
-}
+a"
